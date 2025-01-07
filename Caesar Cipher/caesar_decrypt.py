ced41b50de17f877db4eae0dae21a79685481f6f
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2025-01-08 01:29:24 +0530

    Create caesar_decrypt.py
    deals with the just the decryption part of the caesar cipher algorithm

---
+++ Caesar Cipher/caesar_decrypt.py
@@ -0,0 +1,24 @@
+# Code in Python to implement just the decryption part of Caesar Cipher algorithm
+
+cipher_text = str(input("Enter cipher text: "))
+key = int(input("Enter key: "))
+plain_text = ""
+
+for i in range(0, len(cipher_text)):
+    char = cipher_text[i]
+
+    # to add blank space
+    if char == chr(32):
+        plain_text += char
+        continue
+
+    # for upper-case characters
+    if (char.isupper()):
+        plain_text += chr((ord(char) - key-65) % 26 + 65)
+
+    # for lower-case characters
+    if (char.islower()):
+        plain_text += chr((ord(char) - key-97) % 26 + 97)
+
+print("Cipher Text:	", cipher_text)
+print("Plain Text:	", plain_text)
