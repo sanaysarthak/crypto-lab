df46215999fd7236180506dd5efe301b9887ce48
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2025-01-11 06:25:05 +0000

    added caesar_encrypt.c and reorganized code structure

---
+++ Caesar Cipher/C/encrypt_caesar.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <ctype.h> // for character classification and transformation (example: tolower() )
+
+int main() {
+    char plain_text[100]; 
+    int key;
+    char cipher_text[100];
+    int i;
+
+    // Input plain text
+    printf("Enter plain text: ");
+    scanf("%s", plain_text);
+
+    // Input key
+    printf("Enter key: ");
+    scanf("%d", &key);
+
+    // Encrypting the plain text
+    for (i = 0; plain_text[i] != '\0'; i++) {
+        char char_current = plain_text[i];
+
+        // To add blank space
+        if (char_current == ' ') {
+            cipher_text[i] = char_current;
+            continue;
+        }
+
+        // For upper-case characters
+        if (isupper(char_current)) {
+            cipher_text[i] = (char)((((char_current - 'A') + key) % 26) + 'A');
+        }
+
+        // For lower-case characters
+        else if (islower(char_current)) {
+            cipher_text[i] = (char)((((char_current - 'a') + key) % 26) + 'a');
+        } 
+
+        // Non-alphabetic characters remain unchanged
+        else {
+            cipher_text[i] = char_current; 
+        }
+    }
+
+    cipher_text[i] = '\0'; // Adding null-termination at the end of cipher_text string
+
+    // Display the output
+    printf("Plain Text:  %s\n", plain_text);
+    printf("Cipher Text: %s\n", cipher_text);
+
+    return 0;
+}
